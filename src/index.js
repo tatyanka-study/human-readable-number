be2bd4b0cfb5af9b58bcb88715cbe2bd464d6cfa
tatyana <sazonko.tatyana@gmail.com>
2024-10-09 22:24:33 +0300

    fead: write the function well

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight",
+         "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+         "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",        
+        "eighty", "ninety"];
+
+        
+            if (number === 0) return 'zero';
+
+            if (number < 20) return ones[number];
+
+            if (number < 100) {
+                return tens[Math.floor(number / 10)] + (number % 10 ? ' ' + ones[number % 10] : '');
+            }
+
+            if (number < 1000) {
+                return ones[Math.floor(number / 100)] + ' hundred' + (number % 100 ? ' ' + toReadable(number % 100) : '');
+            }
 }
